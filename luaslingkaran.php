2e9e063440977bda1e00ddc4ef3305b04bb599ab
mridwansaputra27 <mridwansaputra440@gmail.com>
2025-11-11 14:00:03 +0700

    Refactor LuasLingkaran class and add composer.json

--- luaslingkaran.php
+++ luaslingkaran.php
@@ -1,23 +1,10 @@
-<?php
-
-class LuasLingkaran {
-
-    public const phi = 3.14;
-    public int $jari;
-
-    public function tampil($nama = 'ban') {
-        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
-        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+{
+  "name": "username/my-php-app",
+  "description": "Contoh project PHP sederhana dengan namespace dan autoload",
+  "autoload": {
+    "psr-4": {
+      "App\\": "src/"
     }
-
-    public static function testing() {
-        echo "<br/>";
-        echo "ini dari static";
-    }
+  },
+  "require": {}
 }
-
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 12;
-$lingkaran->tampil('roda'); //panggil method tampil
-
-LuasLingkaran::testing(); //panggil static method
