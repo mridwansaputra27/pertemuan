69d31a8b29b571cd2fcf3e527ab8c71a6fcb43e4
mridwansaputra27 <mridwansaputra440@gmail.com>
2025-11-11 14:00:34 +0700

    Refactor LuasLingkaran to Product class
    Refactor LuasLingkaran class into Product class with properties for name and price.

--- luaslingkaran.php
+++ luaslingkaran.php
@@ -1,28 +1,16 @@
 <?php
+namespace App\Models;
 
-namespace App\Math;
+class Product {
+    private string $name;
+    private float $price;
 
-class LuasLingkaran {
-
-    public const phi = 3.14;
-    protected int $jari;
-    
-    public function __construct($isiJari = 1) {
-        $this->jari = $isiJari; //10
+    public function __construct(string $name, float $price) {
+        $this->name  = $name;
+        $this->price = $price;
     }
 
-    public function tampil($nama = 'ban') {
-        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
-        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
-    }
-
-    public static function testing() {
-        echo "<br/>";
-        echo "ini dari static";
-    }
-
-    public function __destruct() {
-        echo "<br/>";
-        echo "udah ah cape";
+    public function getInfo(): string {
+        return "{$this->name} - Rp" . number_format($this->price, 0, ',', '.');
     }
 }
